663b412ffb4c5e7b8a50a5d0d999cc3a255c6232
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-03-22 23:00:09 +1100

    Update script

--- scrollbot.py
+++ scrollbot.py
@@ -4,42 +4,39 @@ import math
 import pyowm
 
 OWM_KEY=''
+BRIGHTNESS=0.1
 
 owm = pyowm.OWM(OWM_KEY)
-observation = owm.weather_at_place('Melbourne,AU')
-weather = observation.get_weather()
 
-status = weather.get_detailed_status()
-temp = weather.get_temperature(unit='celsius')
- 
-message = "{0} {1}".format(
-print(status)
+def getWeather():
+    observation = owm.weather_at_place('Melbourne,AU')
+    weather = observation.get_weather()
+    status = weather.get_detailed_status().capitalize()
+    temp = weather.get_temperature(unit='celsius')
+    msg = "{0}. {1}-{2}   ".format(status, temp['temp_min'], temp['temp_max'])
+    return msg
 
-#sphd.clear()
-#sphd.show()
+def getClock():
+    return time.strftime("%H:%M   ")
 
-#for x in range(17):
-#    sphd.clear()
-#    for y in range(7):
-#        sphd.set_pixel(x, y, 0.25)
-#    sphd.show()
-#    time.sleep(1/17.0)
+sphd.flip(True, True)
 
+display_clock = True
+sphd.write_string(getClock(), brightness=BRIGHTNESS)
 
-#sphd.flip(True, True)
-#txt = "If it's a good idea, go ahead and do it. It is much easier to apologize than it is to get permission. - Grace Hopper"
-#sphd.write_string(txt)
-#while True:
-#    sphd.show()
-#    sphd.scroll(1)
-#    time.sleep(0.05)
+while True:
+    if int(time.time()) % 60 == 0:
+        display_clock = not display_clock
+        sphd.clear()
+        sphd.show()
+        time.sleep(1)
 
+        if display_clock:
+            sphd.write_string(getClock(), brightness=BRIGHTNESS)
+        else:
+            sphd.write_string(getWeather(), brightness=BRIGHTNESS)
 
-#while True:
-#    t = time.time() * 10
-#    for x in range(17):
-#        for y in range(7):
-#            b = math.sin(x + y + t) + math.cos(x + y + t)
-#            b = (b + 2) / 4
-#            sphd.set_pixel(x, y, b)
-#    sphd.show()
+    sphd.scroll(1)
+    sphd.show()
+    time.sleep(0.1)
+
